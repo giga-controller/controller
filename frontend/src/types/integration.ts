c46be2ff6131b70218ffd94aabbd62d89ab85e0f
jinyang628 <chenjinyang4192@gmail.com>
2024-09-21 16:50:23 -0700

    Update

--- frontend/src/types/integration.ts
+++ frontend/src/types/integration.ts
@@ -7,6 +7,7 @@ import {
   SiLinear,
   SiMicrosoftoutlook,
   SiGooglesheets,
+  SiGoogledocs
 } from "react-icons/si";
 import { FaXTwitter } from "react-icons/fa6";
 
@@ -43,6 +44,7 @@ export const integrationIconMapping: IntegrationIconMapping = {
   outlook: SiMicrosoftoutlook,
   x: FaXTwitter,
   sheets: SiGooglesheets,
+  docs: SiGoogledocs
 };
 
 export type AuthParamProps = {
