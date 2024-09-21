7feec631ad1360a8a0d0b9c6dcca22e57ce58ec2
jinyang628 <chenjinyang4192@gmail.com>
2024-09-21 16:52:40 -0700

    Update

--- frontend/src/constants/route.ts
+++ frontend/src/constants/route.ts
@@ -14,5 +14,5 @@ export const ROUTE: Record<string, URL | Route<string>> = {
   Sheets: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/Sheets` as string,
   Linear: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/Linear` as string,
   X: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/X` as string,
-  Docs: `${process.env.NEXT_PUBLIC_DOCS_URL}/docs` as string,
+  Docs: `${process.env.NEXT_PUBLIC_DOCS_URL}/Docs` as string,
 };
