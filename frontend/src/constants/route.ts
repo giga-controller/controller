b91701d8a65d9e5accdd44b63cecbc04cccdbd96
Aarontanzb <aarontanzb@gmail.com>
2024-09-28 14:08:15 -0700

    change setup for Google

--- frontend/src/constants/route.ts
+++ frontend/src/constants/route.ts
@@ -7,13 +7,13 @@ export const ROUTE: Record<string, URL | Route<string>> = {
   docs: process.env.NEXT_PUBLIC_DOCS_URL as string,
 
   // Integration pages in docs
-  Docs: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/google/docs` as string,
+  Docs: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/google/common` as string,
   Gmail:
-    `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/google/gmail` as string,
+    `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/google/common` as string,
   Calendar:
-    `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/google/calendar` as string,
+    `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/google/common` as string,
   Sheets:
-    `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/google/sheets` as string,
+    `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/google/common` as string,
   Slack: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/slack` as string,
   Linear: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/linear` as string,
   X: `${process.env.NEXT_PUBLIC_DOCS_URL}/integrations/x` as string,
