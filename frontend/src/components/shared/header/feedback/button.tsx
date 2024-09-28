edcc1c716ccbea02f13573a0dce6a2cc0587f676
SuveenE <suveen.te1@gmail.com>
2024-09-27 20:38:32 -0700

    Update header styling

--- frontend/src/components/shared/header/feedback/button.tsx
+++ frontend/src/components/shared/header/feedback/button.tsx
@@ -20,7 +20,7 @@ export default function FeedbackButton() {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger
-        className="ml-1 mr-1 px-4 py-2 rounded border border-input bg-green-500 hover:bg-green-800 hover:text-white shadow-sm"
+        className="mx-1 px-4 py-1.5 rounded border border-input bg-green-500 hover:bg-green-700 hover:text-white shadow-sm"
         aria-label="Feedback"
       >
         Feedback
