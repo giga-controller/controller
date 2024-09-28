6ed507741157d0594149cd0fe7f02e1c7c8c7e03
jinyang628 <chenjinyang4192@gmail.com>
2024-09-28 16:52:02 -0700

    Update

--- backend/app/connectors/client/slack.py
+++ backend/app/connectors/client/slack.py
@@ -23,34 +23,34 @@ class SlackClient:
         channels = response["channels"]
         channel_names = [channel["name"] for channel in channels]
         return channel_names
-    
+
     async def get_all_channel_ids(
         self, request: SlackGetChannelIdRequest
     ) -> list[dict[str, Any]]:
         response = await self.client.conversations_list()
         channels = response["channels"]
         request = await self._repair_channel_names(request=request)
-        
+
         channel_info = [
             {"channel_name": channel["name"], "channel_id": channel["id"]}
             for channel in channels
-            if channel["name"]
-            in request.channel_names
+            if channel["name"] in request.channel_names
         ]
         return channel_info
 
-    async def _repair_channel_names(self, request: SlackGetChannelIdRequest) -> SlackGetChannelIdRequest:
+    async def _repair_channel_names(
+        self, request: SlackGetChannelIdRequest
+    ) -> SlackGetChannelIdRequest:
         possible_channel_names: list[str] = await self.get_all_channel_names()
         updated_channel_names: list[str] = []
         for channel_name in request.channel_names:
             updated_channel_names.append(
                 get_most_similar_string(
-                    target=channel_name, 
-                    candidates=possible_channel_names
+                    target=channel_name, candidates=possible_channel_names
                 )
             )
         return SlackGetChannelIdRequest(channel_names=updated_channel_names)
-        
+
     async def send_message(self, request: SlackSendMessageRequest):
         response = await self.client.chat_postMessage(
             channel=request.channel_id, text=request.text
--- backend/app/sandbox/integrations/slack.py
+++ backend/app/sandbox/integrations/slack.py
@@ -25,7 +25,6 @@ async def main():
     ## AGENT TEST
 
 
-
 if __name__ == "__main__":
     asyncio.run(main())
 
