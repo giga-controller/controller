58a6a4a0d47eda3d350a3ee26f5cc278c3360144
jinyang628 <chenjinyang4192@gmail.com>
2024-09-28 13:18:16 -0700

    Update

--- backend/app/connectors/client/x.py
+++ backend/app/connectors/client/x.py
@@ -1,7 +1,10 @@
 import asyncio
-import tweepy
-from app.models.integrations.x import Tweet, XSendTweetRequest
 from functools import partial
+
+import tweepy
+
+from app.models.integrations.x import Tweet, XSendTweetRequest
+
 
 class XClient:
     def __init__(self, access_token: str):
@@ -9,7 +12,9 @@ class XClient:
 
     async def send_tweet(self, request: XSendTweetRequest) -> Tweet:
         loop = asyncio.get_event_loop()
-        create_tweet_partial = partial(self.client.create_tweet, text=request.text, user_auth=False)
+        create_tweet_partial = partial(
+            self.client.create_tweet, text=request.text, user_auth=False
+        )
         response = await loop.run_in_executor(None, create_tweet_partial)
         return Tweet.model_validate(response.data)
 
